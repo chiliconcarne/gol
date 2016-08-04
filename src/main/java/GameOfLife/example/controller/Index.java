189ca3c98259edc70152e9101db4c10b012b3181
sernowm <Skr!pto201K9>
2016-08-04 15:31:24 +0200

    Bugfix

--- src/main/java/GameOfLife/example/controller/Index.java
+++ src/main/java/GameOfLife/example/controller/Index.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by sernowm on 04.08.2016.
@@ -12,9 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Index implements ErrorController {
     @RequestMapping("/")
     public String index(Model model){
-        model.addAttribute("message","Index");
-        System.err.println("Index");
-        return "main";
+        return "login";
+    }
+
+    @RequestMapping("/profile")
+    public String profile(
+            @RequestParam(name = "username", required = false, defaultValue = "Max") String username, Model model
+    ){
+        model.addAttribute("username",username);
+        return "profile";
+    }
+
+    @RequestMapping("/conway")
+    public String conway(Model model){
+        return "conway";
     }
 
     @RequestMapping("/error")
