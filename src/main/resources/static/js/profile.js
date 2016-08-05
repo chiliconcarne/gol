6203497a2fba324b10b665de7197f281f12ed4e5
sernowm <Skr!pto201K9>
2016-08-05 14:05:24 +0200

    Profil bearbeitet

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -11,7 +11,14 @@ $(document).ready(function(){
         else
             $("#start").text(wait);
     });
-    $(".color").on('hover',function(){
-        $(this).css('width','200px');
+    $(".color").on('mouseover',function(){
+        $(this).css('width','70px');
+        $(this).css('height','70px');
+        $(this).css('marginTop','10px');
+    });
+    $(".color").on('mouseout',function(){
+        $(this).css('width','50px');
+        $(this).css('height','50px');
+        $(this).css('marginTop','30px');
     });
 })
