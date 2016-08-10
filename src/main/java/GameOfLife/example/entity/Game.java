38305bb2bd5d2a93b2da51f4ea013a03b007d7b1
sernowm <martin.sernow@arcor.de>
2016-08-10 18:52:57 +0200

    Websocket

--- src/main/java/GameOfLife/example/entity/Game.java
+++ src/main/java/GameOfLife/example/entity/Game.java
@@ -1,22 +1,29 @@
 package GameOfLife.example.entity;
 
-import GameOfLife.example.repository.GameRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
 /**
  * Created by sernowm on 10.08.2016.
  */
+@Entity
 public class Game {
     @Id
     int id;
     String spieler1,spieler2;
+    private int[][] board = new int[30][30];
+
+    public Game(){}
 
     public Game(int id, String spieler1, String spieler2) {
         this.id = id;
         this.spieler1 = spieler1;
         this.spieler2 = spieler2;
+        for(int y = 0; y < 30; y++){
+            for(int x = 0; x < 30; x++){
+                board[y][x]=0;
+            }
+        }
     }
 
     public int getId() {
@@ -41,5 +48,13 @@ public class Game {
 
     public void setSpieler2(String spieler2) {
         this.spieler2 = spieler2;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(int[][] board) {
+        this.board = board;
     }
 }
