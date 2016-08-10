3604babca64bd167c9a9f7456740266c10c7d474
sernowm <martin.sernow@arcor.de>
2016-08-10 17:59:23 +0200

    Websocket

--- src/main/java/GameOfLife/example/json/Board.java
+++ src/main/java/GameOfLife/example/json/Board.java
@@ -7,6 +7,7 @@ import java.util.Random;
  */
 public class Board {
     private int[][] board = new int[30][30];
+    private String spieler1;
     public Board(){
         Random rnd = new Random();
         for(int y = 0; y < 30; y++){
@@ -14,11 +15,24 @@ public class Board {
                 board[y][x]=rnd.nextInt(8);
             }
         }
+        spieler1 = "kai";
+    }
+    public Board(String spieler){
+        Random rnd = new Random();
+        for(int y = 0; y < 30; y++){
+            for(int x = 0; x < 30; x++){
+                board[y][x]=rnd.nextInt(8);
+            }
+        }
+        spieler1 = spieler;
     }
     public int[][] getBoard() {
         return board;
     }
     public void setBoard(int[][] board) {
         this.board = board;
+    }
+    public String getSpieler1(){
+        return this.spieler1;
     }
 }
