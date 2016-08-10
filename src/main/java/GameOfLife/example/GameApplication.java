c956b174237ad2b5365d7a916e9181e4ff27c91e
sernowm <martin.sernow@arcor.de>
2016-08-10 17:56:23 +0200

    Websocket

--- src/main/java/GameOfLife/example/GameApplication.java
+++ src/main/java/GameOfLife/example/GameApplication.java
@@ -7,9 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @SpringBootApplication
+@EnableScheduling
 public class GameApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GameApplication.class, args);
--- src/main/java/GameOfLife/example/controller/gameController.java
+++ src/main/java/GameOfLife/example/controller/gameController.java
@@ -2,8 +2,11 @@ package GameOfLife.example.controller;
 
 import GameOfLife.example.json.Board;
 import GameOfLife.example.json.Position;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -11,6 +14,8 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class gameController {
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
     @MessageMapping("/ready")
     @SendTo("/game/board")
     public Board ready() throws Exception {
@@ -20,5 +25,9 @@ public class gameController {
     @SendTo("/game/board")
     public Board set(Position pos) throws Exception {
         return new Board();
+    }
+    @Scheduled(fixedRate = 500)
+    public void update(){
+        this.messagingTemplate.convertAndSend("/game/board",new Board());
     }
 }
