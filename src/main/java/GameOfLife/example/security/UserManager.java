50292af332c4c7a77169bd35714704e2eb3e05ae
sernowm <martin.sernow@arcor.de>
2016-08-19 12:40:36 +0200

    Passwort Validation eingefügt mit Properties in den eigenen Dateien

--- src/main/java/GameOfLife/example/security/UserManager.java
+++ src/main/java/GameOfLife/example/security/UserManager.java
@@ -5,36 +5,73 @@ import GameOfLife.example.repository.ProfilRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Properties;
+import java.util.regex.Pattern;
 
 /**
  * Created by raedschk on 09.08.2016.
  */
 public class UserManager {
 
-    @Autowired
-    InMemoryUserDetailsManager inMemoryUserDetailsManager;
+    private static Properties p = new Properties();
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @Autowired
-    ProfilRepository pRepo;
-
-    public boolean createNewUser(String username, String password)
-    {
-        if(!inMemoryUserDetailsManager.userExists(username))
-        {
-            inMemoryUserDetailsManager.createUser(new User(username, passwordEncoder.encode(password), new ArrayList<GrantedAuthority>()));
-            pRepo.save(new Profil(username, 1, 2, 20, 20, 50));
-            return true;
+    static {
+        boolean create = false;
+        String secutiryProp = System.getProperty("user.home").concat("\\gol\\security.properties");
+        FileInputStream in;
+        FileWriter out;
+        try {
+            in = new FileInputStream(secutiryProp);
+            p.load(in);
+        } catch (FileNotFoundException e) {
+            try {
+                out = new FileWriter(secutiryProp);
+                p.setProperty("passwordMinLength", "5");
+                p.setProperty("passwordMaxLength", "12");
+                p.setProperty("passwordAllowSpecialChars", "false");
+                new File(secutiryProp).mkdirs();
+                p.store(out, "Zum Ändern der Passworteinstellungen");
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-        else
-        {
-            return false;
+    }
+
+    @Autowired
+    private InMemoryUserDetailsManager inMemoryUserDetailsManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ProfilRepository pRepo;
+
+    public boolean createNewUser(String username, String password) {
+        if(validatPassword(password)) {
+            if (!inMemoryUserDetailsManager.userExists(username)) {
+                inMemoryUserDetailsManager.createUser(new User(username, passwordEncoder.encode(password), new ArrayList<GrantedAuthority>()));
+                pRepo.save(new Profil(username, 1, 2, 20, 20, 50));
+                return true;
+            }
         }
+        return false;
+    }
+
+    private boolean validatPassword(String password) {
+        return  password.length()>=Integer.valueOf(p.getProperty("passwordMinLength","5")) &&
+                password.length()<=Integer.valueOf(p.getProperty("passwordMaxLength","12")) &&
+                (p.getProperty("passwordAllowSpecialChars","false").equals("true") ? true : Pattern.compile("[a-zA-Z0-9]+").matcher(password).matches());
+    }
+
+    private UserDetails getUserByName(String name) {
+        return inMemoryUserDetailsManager.loadUserByUsername(name);
     }
 }
--- src/test/java/GameOfLife/example/security/UserManagerTest.java
+++ src/test/java/GameOfLife/example/security/UserManagerTest.java
@@ -1,16 +1,80 @@
 package GameOfLife.example.security;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import java.io.*;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.After;
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.*;
 
-/**
- * Created by sernowm on 10.08.2016.
- */
+
 public class UserManagerTest {
+
+    private static Properties p = new Properties();
+
+    static {
+        boolean create = false;
+        String secutiryProp = System.getProperty("user.home").concat("\\gol\\security.properties");
+        FileInputStream in;
+        FileWriter out;
+        try {
+            in = new FileInputStream(secutiryProp);
+            p.load(in);
+        } catch (FileNotFoundException e) {
+            try {
+                out = new FileWriter(secutiryProp);
+                p.setProperty("passwordMinLength", "5");
+                p.setProperty("passwordMaxLength", "12");
+                p.setProperty("passwordAllowSpecialChars", "false");
+                new File(secutiryProp).mkdirs();
+                p.store(out, "Zum Ändern der Passworteinstellungen");
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
     @Test
     public void createNewUser() throws Exception {
+        // Zu kurz
+        assertTrue(validatPassword("12345"));
+        assertFalse(validatPassword("1234"));
 
+        // Sonderzeichen
+        assertTrue(validatPassword("Maximilian"));
+        assertFalse(validatPassword("Max Kruse"));
+
+        // Zahlen / Groß- / Kleinbuchstaben
+        assertTrue(validatPassword("Skr12P45"));
+        assertFalse(validatPassword("Skr!Pto201K9"));
+
+        // Zu Lang
+        assertTrue(validatPassword("JOCHIELCO35F"));
+        assertFalse(validatPassword("JOCHIELCO35F3"));
+    }
+
+    private boolean validatPassword(String password) {
+        return  password.length()>=Integer.valueOf(p.getProperty("passwordMinLength","5")) &&
+                password.length()<=Integer.valueOf(p.getProperty("passwordMaxLength","12")) &&
+                (p.getProperty("passwordAllowSpecialChars","false").equals("true") ? true : Pattern.compile("[a-zA-Z0-9]+").matcher(password).matches());
     }
 
 }
